c9cd5c3344ce93637c87ec02326a44501538bb91
Siddharth3710 <143256569+Siddharth3710@users.noreply.github.com>
2025-07-02 12:02:58 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Count Primes In Range/count-primes-in-range.java
@@ -0,0 +1,23 @@
+class Solution {
+    int countPrimes(int l, int r) {
+        int cnt = 0;
+        for (int num = l; num <= r; num++) {
+            if (isPrime(num)) {
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+
+    boolean isPrime(int n) {
+        if (n <= 1) return false;
+        if (n == 2 || n == 3) return true;
+        if (n % 2 == 0 || n % 3 == 0) return false;
+
+        for (int i = 5; i * i <= n; i += 6) {
+            if (n % i == 0 || n % (i + 2) == 0)
+                return false;
+        }
+        return true;
+    }
+}
